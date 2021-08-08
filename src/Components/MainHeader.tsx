594f1d65b63b825013d3e50d6b6c08d5c2b9b1a9
Matt Trueblood <mtrueblood3@gmail.com>
2021-08-08 17:50:33 -0400

    Creates a header component with a menu.

---
+++ src/Components/MainHeader.tsx
@@ -0,0 +1,28 @@
+import menuIcon from "../Assets/Icons/menu.svg";
+import {Transition} from "@tailwindui/react";
+import React, {useState} from "react";
+
+const MainHeader = () => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    return(<>
+        <header className="flex p-5 justify-between">
+            <div className={'text-xl text-warmGrey-900 font-bold border-b-2 border-warmGrey-900 flex-initial'}>Color Cloud & Sound</div>
+            <button className={'flex-2 bg-green-500'} onClick={() => setIsOpen(!isOpen)}><img src={menuIcon} alt={'Menu'}/></button>
+        </header>
+    <Transition show={isOpen}
+                enter="transition-opacity duration-75"
+                enterFrom="opacity-0"
+                enterTo="opacity-100 bg-warmGrey-200"
+                leave="transition-opacity duration-150"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+                className={''}>
+        <div className={'p-5 bg-warmGrey-200'}>
+            Contact
+        </div>
+    </Transition>
+        </>
+    )
+}
+export default MainHeader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      grey: colors.trueGray,
+      warmGrey: colors.warmGray,
+      red: colors.red,
+      yellow: colors.amber,
+      blue: colors.blue
+    },
     extend: {
       fontFamily: {
-        'montserrat': ['"Montserrat"', 'sans-serif']
+        'mont': ['"Montserrat"', 'sans-serif']
       }
     }
   },
